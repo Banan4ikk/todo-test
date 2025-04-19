06326d074f725a4610edb800db425911901a517c
Константин Лотов <banan03112000@gmail.com>
2025-04-19 13:53:05 +0600

    init commit front

---
+++ client/src/App.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders To-Do List header', () => {
+  render(<App />);
+  const headerElement = screen.getByText(/To-Do List/i);
+  expect(headerElement).toBeInTheDocument();
+});
---
+++ client/src/App.tsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
+type Todo = {
+  id: number,
+  text: string,
+}
+
+function App() {
+  const [todos, setTodos] = useState<Array<Todo>>([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/todos')
+      .then(res => setTodos(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
+  const addTodo = () => {
+    axios.post('http://localhost:5000/api/todos', { title: newTodo })
+      .then(res => setTodos([...todos, res.data]))
+      .catch(err => console.error(err));
+    setNewTodo('');
+  };
+
+  const deleteTodo = (id: number) => {
+    axios.delete(`http://localhost:5000/api/todos/${id}`)
+      .then(() => setTodos(todos.filter(todo => todo.id !== id)))
+      .catch(err => console.error(err));
+  };
+
+  return (
+    <div className="App">
+      <h1>To-Do List</h1>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        placeholder="Название задачи"
+      />
+      <button onClick={addTodo}>Добавить задау</button>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            {todo.text}
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
